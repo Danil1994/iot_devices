2fe5c31726d29c53bf43e53e7323989ea43bd616
Danil1994 <danilaleksandrovich36@gmail.com>
2024-07-26 16:13:17 +0300

    config db

--- db/config_db.py
+++ db/config_db.py
@@ -5,13 +5,13 @@ from peewee import PostgresqlDatabase, SqliteDatabase
 
 load_dotenv()
 
-db = SqliteDatabase('app.db')
+# db = SqliteDatabase('app.db') <- uncomment if you wont use SQLite DB and comment code below
 
 
-# db = PostgresqlDatabase(
-#     os.getenv('DATABASE'),
-#     user='postgres',
-#     password=os.getenv('DATABASW_PASSWORD'),
-#     host='localhost',
-#     port=5432
-# )
+db = PostgresqlDatabase(
+    os.getenv('DATABASE'),
+    user='postgres',
+    password=os.getenv('DATABASW_PASSWORD'),
+    host='localhost',
+    port=5432
+)
